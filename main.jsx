7ff78337c1267de0b2cd8741bc6b989d1e505393
Lucy <lucy_oswald@hotmail.co.uk>
2023-06-03 16:58:57 +0100

    hello

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "App.jsx";
 import "../sass/styles.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-tabs/style/react-tabs.css";
