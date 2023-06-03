4f037c18dd4a00621c24cc496c415191fe38fd5e
Lucy <lucy_oswald@hotmail.co.uk>
2023-06-03 17:21:22 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Typist from "react-typist-component";
-import About from "./components/About";
+import About from "../components/About";
 import { Link } from "react-scroll";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import logo from "./assets/128851704_padded_logo-removebg-preview.png";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import logo from "../assets/128851704_padded_logo-removebg-preview.png";
 import { useEffect, useRef, useState } from "react";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./sass/styles.scss";
+import "../sass/styles.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-tabs/style/react-tabs.css";
-import Home from "./Home";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Home />
+    <App />
   </BrowserRouter>
 );
